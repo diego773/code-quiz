6e51632b5cc5e963db2cb8719c5cb9c67401268d
Diego <diegovillarreal123@gmail.com>
2021-02-24 15:57:22 -0600

    trying to display my questions

--- logic.js
+++ logic.js
@@ -6,21 +6,19 @@ var time = questions.length * 15
 var timerEl = document.getElementById("time");
 
 
+
 function startQuiz() {
     startScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
 
     timerId = setInterval(clockTick, 1000)
     timerEl.textContent = time;
-    var question = getQuestion("What is the closest planet to Jupiter?", "How much does basketball weight?", 
-    "Who was the first man on the moon?", "Who played Batman in the 1992 Batman Returns?",
-    "Who sang Ready or Not?"); 
-
+    
+    //getQuestions();
 }
 
 
 function clockTick() {
-    console.log(time);
     time--;
     timerEl.textContent = time;
     if (time <= 0 ) {
