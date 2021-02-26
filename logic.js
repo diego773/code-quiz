18afdbd351a3c83ab8d2f2f87de51e6baa101b71
Diego <diegovillarreal123@gmail.com>
2021-02-25 21:30:54 -0600

    worked on css

--- logic.js
+++ logic.js
@@ -4,26 +4,48 @@ var questionsEl = document.getElementById("questions");
 var timerId;
 var time = questions.length * 15
 var timerEl = document.getElementById("time");
-var question = document.createElement("question");
+var question = document.createElement("p");
+    question.innerText = "";
+var choicesId;
+var choicesEl = document.getElementById("choices");
+var choices = document.createElement("p");
+var answerId;   
+var answer = document.createElement("p");
 
+
+
+// startQuiz functions
 function startQuiz() {
     startScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
     
     timerId = setInterval(clockTick, 1000)
     timerEl.textContent = time;
+
+// Append questions
+    questionsEl.appendChild(question);
     
-// Append child    
-    questionsEl.appendChild(question); 
-    for (var i = 0; i <= questions.length; i++);
-    
+    choicesEl.appendChild(choices)
+
+// Loops questions    
+    for ( i = 0; i < questions.length; i++);
+
 
 }
 
+// Adds buttons to the questions    
+    // questionsEl.addEventListener("click", question);
+
+
+
+
+// Timer clock
 function clockTick() {
     time--;
     timerEl.textContent = time;
     if (time <= 0 ) {
+
+
         //quizEnd()
     }
 
