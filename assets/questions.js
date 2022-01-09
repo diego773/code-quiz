72de39d1c9d9606976257251ab0486ca2f4b8a47
Diego <diegovillarreal123@gmail.com>
2022-01-09 00:38:47 -0600

    update questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -1,18 +1,20 @@
-var questions = [
+// Array questions
+const questions = [
   {
-    title: "Commonly used data types DO NOT include:",
+    question: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts",
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses",
   },
   {
-    title: "Arrays in JavaScript can be used to store ____.",
+    question: "Arrays in JavaScript can be used to store ____.",
     choices: [
-      "numbers and strings",
+      "number and strings",
       "other arrays",
       "booleans",
       "all of the above",
@@ -20,13 +22,13 @@ var questions = [
     answer: "all of the above",
   },
   {
-    title:
+    question:
       "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes",
   },
   {
-    title:
+    question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
     answer: "console.log",
